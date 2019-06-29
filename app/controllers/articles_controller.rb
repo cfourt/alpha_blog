c07f55648afa058cb3ab5dcfb8d00cffb22671ef
Connor <connorfourt@gmail.com>
2019-06-29 12:02:50 -0700

    Added index, delete action, partials, and links.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,8 +18,30 @@ class ArticlesController < ApplicationController
     @article = Article.find params[:id]
   end
 
-  def index
+  def edit
+    @article = Article.find params[:id]
+  end
 
+  def update
+    @article = Article.find params[:id]
+
+    if @article.update(article_params)
+      flash[:success] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def destroy
+    @article = Article.find params[:id]
+    @article.destroy
+    flash[:success] = "Articles was successfully deleted"
+    redirect_to articles_path
   end
 
   private
