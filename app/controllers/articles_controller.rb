a2ba756d7401e150578df675f5767b7cc6f46bf8
Connor <connorfourt@gmail.com>
2019-08-17 20:09:10 -0700

    Darkmode v0.1, overrides. Create show action for users and gravatar.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -51,6 +51,6 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, :user_id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find params[:id]
   end
 
   # GET /users/new
@@ -28,7 +29,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        flash[:success] = "User was successfully created, welcome to the blog #{@user.username}"
+        format.html { redirect_to @user }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
@@ -42,7 +44,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        flash[:success] = "User was successfully updated, welcome to the blog #{@user.username}"
+        flash[:success] = "User was successfully updated"
         format.html {redirect_to @user}
         format.json { render :show, status: :ok, location: @user }
       else
